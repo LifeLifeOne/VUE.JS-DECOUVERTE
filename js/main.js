9fcad4c08b6ac67ee079b05da734d55483c677db
Vivien <vivien.barbeau.contact@gmail.com>
2022-01-04 17:15:09 +0100

    update image and stock msg based on active variant

--- js/main.js
+++ js/main.js
@@ -3,23 +3,27 @@ const app = Vue.createApp({
         return {
             product: 'iPhone 13',
             description: 'Un système photo nettement plus puissant. Un écran si réactif qu’il renouvelle vos sensations à chaque interaction.',
-            src: './assets/images/iphone-13-blue.png',
-            available: false,
             specs: ['Waterproof', 'Écran HDR', 'Haptic Touch'],
             cart: 0,
             variants: [
-                { id: 'iphone-13-001', color: 'Bleu', pillColor: '#1A5F7A', image: './assets/images/iphone-13-blue.png' },
-                { id: 'iphone-13-002', color: 'Noir', pillColor: '#111', image: './assets/images/iphone-13-midnight.png' },
-                { id: 'iphone-13-003', color: 'Rouge', pillColor: '#FF5959', image: './assets/images/iphone-13-red.png' }
-            ]
+                { id: '1', quantity: 12, color: 'Bleu', pillColor: '#1A5F7A', image: './assets/images/iphone-13-blue.png' },
+                { id: '2', quantity: 0, color: 'Noir', pillColor: '#111', image: './assets/images/iphone-13-midnight.png' },
+                { id: '3', quantity: 0, color: 'Rouge', pillColor: '#FF5959', image: './assets/images/iphone-13-red.png' }
+            ],
+            activeVariant: 0
         }
     },
     methods: {
-        addToCart() {
-            this.cart++;
-        },
         colorChange(imageUrl) {
-            this.src = imageUrl;
+            this.image = imageUrl;
+        }
+    },
+    computed: {
+        image() {
+            return this.variants[this.activeVariant].image
+        },
+        inStock() {
+            return this.variants[this.activeVariant].quantity > 0 ? true : false
         }
     }
 });
